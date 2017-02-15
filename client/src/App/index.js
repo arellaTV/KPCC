985308490b66bb4578bd4d28886adb035480228d
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-02-14 17:34:32 -0800

    Renders list of articles based off of example query

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -2,8 +2,34 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      articles: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://www.scpr.org/api/v3/articles?query=virgin+galactic')
+      .then(response => response.json())
+      .then(body => {
+        this.setState({ articles: body.articles })
+      })
+  }
+
   render() {
-    return <p>Webpack test!</p>
+    return (
+      <div>Webpack test!!
+        <ul>
+        {this.state.articles.map(article => {
+          return (
+            <li>{article.title}</li>
+          );
+        })}
+        </ul>
+      </div>
+    )
   }
 }
 
