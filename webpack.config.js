0e92dc82e2849e16580522e96a2dba5195df74b2
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-02-14 16:50:36 -0800

    Adds babel loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,19 @@ const BUILD_DIR = `${__dirname}public`;
 const APP_DIR = `${__dirname}client/src/App`;
 
 const config = {
-  entry: `${APP_DIR}/index.jsx`,
+  entry: `${APP_DIR}/index.js`,
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel'
+      }
+    ]
   }
 };
 
