81a0ad7de5106186965a6100c7f524a79a549f2f
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-02-14 17:42:19 -0800

    Renders input fields

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -20,7 +20,13 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>Webpack test!!
+      <div>
+        <h1>KPCC</h1>
+        <h3>Search Articles</h3>
+        <form>
+          <input type='text' />
+          <input type='submit'/>
+        </form>
         <ul>
         {this.state.articles.map((article, index) => {
           return (
