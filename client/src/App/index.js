6bd5bab84f5dbed8b6ce6632840fc94c3be94a16
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-02-14 17:51:39 -0800

    Defines function that fetches based off of query

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -8,14 +8,25 @@ class App extends React.Component {
       query: '',
       articles: []
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-    fetch('http://www.scpr.org/api/v3/articles?query=virgin+galactic')
+    this.getArticlesByQuery('virgin galactic');
+  }
+
+  getArticlesByQuery(query) {
+    const queryStringWithPlus = query.split(' ').join('+');
+    fetch(`http://www.scpr.org/api/v3/articles?query=${queryStringWithPlus}`)
       .then(response => response.json())
       .then(body => {
         this.setState({ articles: body.articles })
       })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(event.target[0].value);
   }
 
   render() {
@@ -23,7 +34,7 @@ class App extends React.Component {
       <div>
         <h1>KPCC</h1>
         <h3>Search Articles</h3>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input type='text' />
           <input type='submit'/>
         </form>
