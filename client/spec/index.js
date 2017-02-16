e51fda4e26184a665ec1e7b27eed503691b390fa
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-02-15 17:51:02 -0800

    Adds fetch polyfill to node global

--- client/spec/index.js
+++ client/spec/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
+import 'isomorphic-fetch';
 
 import jsdom from 'jsdom';
 const doc = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
@@ -13,7 +14,7 @@ import SearchBar from './../src/App/SearchBar';
 import ArticleEntry from './../src/App/ArticleEntry';
 
 describe('<App />', () => {
-  it ('calls componentDidMount', () => {
+  it('calls componentDidMount', () => {
     sinon.spy(App.prototype, 'componentDidMount');
     const wrapper = mount(<App />);
     expect(App.prototype.componentDidMount.calledOnce).to.equal(true);
--- client/src/App/index.js
+++ client/src/App/index.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    // this.getArticlesByQuery(this.state.keywords);
+    this.getArticlesByQuery(this.state.keywords);
   }
 
   getArticlesByQuery(keywords) {
